64365f3ab5d4ebb9aa27441f0538f4e5053056a5
Yael Donat <yael.donat@infotel.com>
2024-03-01 15:33:12 +0100

    change front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoInputComponent,
+    TodoItemComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
