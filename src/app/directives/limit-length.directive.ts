250297d163445818c2afb9fdbaef29bd79aa4292
Yael Donat <yael.donat@infotel.com>
2024-03-05 15:21:09 +0100

    fix css, to continue

--- src/app/directives/limit-length.directive.ts
+++ src/app/directives/limit-length.directive.ts
@@ -1,15 +1,75 @@
-import { Directive, HostListener, ElementRef } from '@angular/core';
+// import { Directive, HostListener, ElementRef, Renderer2 } from '@angular/core';
+
+// @Directive({
+//     selector: '[appLimitLength]'
+// })
+// export class LimitLengthDirective {
+//     private originalValue: string = '';
+
+//     constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+//     @HostListener('blur') onBlur() {
+//         const value: string = this.el.nativeElement.value;
+//         if (value.length > 20) {
+//             console.log(value);
+//             console.log(this.originalValue);
+            
+//             this.originalValue = value;
+//             console.log(this.originalValue);
+//             console.log(this.el.nativeElement);
+//             this.renderer.setProperty(this.el.nativeElement, 'value', value.substring(0, 20) + '...');
+//         }
+//     }
+
+//     @HostListener('focus') onFocus() {
+//         if (this.originalValue) {
+//             console.log(this.originalValue);
+//             console.log(this.el.nativeElement);
+//             this.renderer.setProperty(this.el.nativeElement, 'value', this.originalValue);
+//         }
+//     }
+
+//     @HostListener('input') onInput() {
+//         const value: string = this.el.nativeElement.value;
+//         if (value.length <= 20) {
+//             console.log(this.originalValue);
+//             console.log(value);
+//             this.originalValue = value;
+//         }
+//     }
+// }
+
+
+import { Directive, HostListener, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
     selector: '[appLimitLength]'
 })
 export class LimitLengthDirective {
+    private initialValue: string = '';
 
-    constructor(private el: ElementRef) { }
+    constructor(private el: ElementRef, private renderer: Renderer2) {
+        this.initialValue = this.el.nativeElement.value;
+    }
 
-    @HostListener('input', ['$event.target.value']) onInput(value: string) {
-        if (value.length > 20) {
-            this.el.nativeElement.value = value.substring(0, 20) + '...';
+    @HostListener('blur') onBlur() {
+        if (this.initialValue.length > 20) {
+            console.log(this.initialValue)
+            this.renderer.setProperty(this.el.nativeElement, 'value', this.initialValue.substring(0, 20) + '...');
         }
+    }
+
+    @HostListener('focus') onFocus() {
+        console.log(this.initialValue)
+        this.renderer.setProperty(this.el.nativeElement, 'value', this.initialValue);
+    }
+
+    @HostListener('input') onInput() {
+        console.log(this.el.nativeElement.value)
+        this.initialValue = this.el.nativeElement.value;
+    }
+
+    @HostListener('submit', ['$event']) onFormSubmit(event: Event) {
+        this.initialValue = '';
     }
 }
--- src/app/interfaces/todo.interface.ts
+++ src/app/interfaces/todo.interface.ts
@@ -10,8 +10,67 @@ export enum State {
     FINISHED = "Finished"
 }
 
-export let initialTodos: TodoModel[] = [{
-    id: '0',
-    title: "Default task",
-    state: State.NOT_STARTED
-}]
+export let initialTodos: TodoModel[] = [
+    {
+        id: '0',
+        title: "Default task",
+        state: State.NOT_STARTED
+    },
+    {
+        id: '1',
+        title: "Default task",
+        state: State.NOT_STARTED
+    },
+    {
+        id: '2',
+        title: "Default task",
+        state: State.NOT_STARTED
+    },
+    {
+        id: '3',
+        title: "Default task",
+        state: State.NOT_STARTED
+    },
+    {
+        id: '4',
+        title: "Default task",
+        state: State.NOT_STARTED
+    },
+    {
+        id: '4',
+        title: "Default task",
+        state: State.NOT_STARTED
+    },
+    {
+        id: '5',
+        title: "Default task",
+        state: State.NOT_STARTED
+    },
+    {
+        id: '6',
+        title: "Default task",
+        state: State.NOT_STARTED
+    },
+    {
+        id: '7',
+        title: "Default task",
+        state: State.NOT_STARTED
+    },
+    {
+        id: '8',
+        title: "Default task",
+        state: State.NOT_STARTED
+    },
+    {
+        id: '9',
+        title: "Default task",
+        state: State.NOT_STARTED
+    },
+    {
+        id: '10',
+        title: "Default task",
+        state: State.NOT_STARTED
+    },
+
+
+]
